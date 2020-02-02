d0478108da9157ac27ff4082f7803e5c88f6375d
h81183gb <gurneet.bhatia@student.manchester.ac.uk>
2020-02-02 14:59:32 +0000

    gets input and validates it to see if it fits on board

--- chess/CheckInput.java
+++ chess/CheckInput.java
@@ -1,7 +1,16 @@
 
 public class CheckInput {
-	
+
     public boolean checkCoordinateValidity(String input){
+        if (input.length() == 2) {
+            input = input.toLowerCase();
+            int row = 0, col = 0;
+            if(Character.isDigit(input.charAt(0)))
+                row = Character.getNumericValue(input.charAt(0));
+            if(Character.isLetter(input.charAt(1)))
+                col = input.charAt(1);
+            return (row > 0) && (row < 9) && (col >= 'a') && (col <= 'h');
+        }
         return false;
     }
 }
--- chess/Game.java
+++ chess/Game.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Game {
     private static boolean gameEnd=false;
 
@@ -6,13 +6,32 @@ public class Game {
         Board b = new Board();
         b.initialisePieces();
         b.printBoard();
+        Scanner sc = new Scanner(System.in);
         while (!gameEnd){
             //write the game logic
+            System.out.println("--------- Whites move --------");
+            String origin = getCoordinatesInput("> Enter Origin");
+            String destination = getCoordinatesInput("> Enter Destination");
             
-        }		
+        }       
     }
 
     public static void main (String args[]){
         Game g  = new Game();
+    }
+    
+    public static String getCoordinatesInput(String displayString) {
+        Scanner sc = new Scanner(System.in);
+        CheckInput checker = new CheckInput();
+        String output = "";
+        while(output.isEmpty()) {
+            System.out.println(displayString);
+            String input = sc.next();
+            if(checker.checkCoordinateValidity(input))
+                output = input;
+            else
+                System.out.println("ERROR: Please enter valid coordinates between 1a and 8h");
+        }
+        return output;
     }
 }
